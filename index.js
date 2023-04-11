7588ae8452ad673c0d8426d72482e35dec774f63
shetty-nithin <shettynithin744@gmail.com>
2023-04-11 23:57:47 +0530

    disabling the CORS middleware to check deployement issue

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ app.use((req, res, next) => {
     next();
 });
 app.use(express.json());
-app.use(cors({
-    origin: indexConfig.CLIENT_URL,
-    credentials: true  
-}));
+// app.use(cors({
+//     origin: indexConfig.CLIENT_URL,
+//     credentials: true  
+// }));
 app.use(cookieParser());
 
 const storage = multer.diskStorage({
