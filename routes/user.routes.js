57119aa7d7070f6edc60d434ace604acf2af3d63
shetty-nithin <shettynithin744@gmail.com>
2023-04-16 17:16:35 +0530

    adding the authJWT.verifyToken again

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,7 +5,7 @@ import { authJWT } from "../middlewares/index.js"
 const router = express.Router();
 
 router.get("/find/:userId", userController.findUser);
-router.get("/", userController.getUsers);
+router.get("/", [authJWT.verifyToken], userController.getUsers);
 router.put("/", [authJWT.verifyToken], userController.updateUser);
 
 export default router;
