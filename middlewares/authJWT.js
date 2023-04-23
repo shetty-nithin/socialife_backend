a9337c5f24fc7c46db1230dc42e5bc29b50f508b
shetty-nithin <shettynithin744@gmail.com>
2023-04-23 13:56:04 +0530

    small change

--- middlewares/authJWT.js
+++ middlewares/authJWT.js
@@ -2,21 +2,22 @@ import jwt from "jsonwebtoken";
 import authConfig from "../configs/auth.config.js";
 
 const verifyToken = (req, res, next) => {
-    let token = req.headers.cookie;
-    if(token !== undefined){
-        token = token.replace("accessToken=", "");
-    }
+    // let token = req.headers.cookie;
+    // if(token !== undefined){
+    //     token = token.replace("accessToken=", "");
+    // }
     
-    if(!token){
-        return res.status(403).send("You don't have a token. Please login first.");
-    }
-    jwt.verify(token, authConfig.secretKey, async (err, decoded) => {
-        if(err || !decoded.exp || decoded.exp < Date.now()/1000){
-            return res.status(403).send("Your are not authorized!")
-        }
-        req.decodedUserId = decoded.id
-        next();
-    })
+    // if(!token){
+    //     return res.status(403).send("You don't have a token. Please login first.");
+    // }
+    // jwt.verify(token, authConfig.secretKey, async (err, decoded) => {
+    //     if(err || !decoded.exp || decoded.exp < Date.now()/1000){
+    //         return res.status(403).send("Your are not authorized!")
+    //     }
+    //     req.decodedUserId = decoded.id
+    //     next();
+    // })
+    next();
 }
 
 export default { verifyToken };
